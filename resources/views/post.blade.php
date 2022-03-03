92ca07d50efe23bc26d168550efe8446958396d2
jacqueshovine <hovine.jacques@gmail.com>
2022-03-03 15:19:38 +0100

    add progress up to section 2 lesson 7 of tutorial

---
+++ resources/views/post.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+
+<title>My blog</title>
+
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+        <h1><a href="/post">My first post</a></h1>
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur quas possimus voluptatibus, aliquam, nesciunt numquam quasi provident at, dignissimos aut quos asperiores quisquam optio autem laboriosam laudantium totam. Itaque, adipisci!
+        </p>
+    </article>
+
+    <a href="/">Go back</a>
+</body>
