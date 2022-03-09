d0f18a1a625258d8af27df61d273f390a6b464b3
jacqueshovine <hovine.jacques@gmail.com>
2022-03-09 21:19:15 +0100

    add properties fillable and guarded to Post Model
    Section 4 lesson 22 of laracasts from scratch tuto

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    // Prevents mass assignment for following keys
+    // We have to be careful here not to forget a key
+    // Guarded with an empty array disables mass assignment completely
+    protected $guarded = ['id'];
+
+    // Allows mass assignment for following keys
+    // This is more explicit, and prevents from missing something
+    protected $fillable = ['title', 'excerpt', 'body'];
 }
