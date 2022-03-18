41ba3e26efce5a80644c0da42e55ddef9e2842f3
jacques <hovine.jacques@gmail.com>
2022-03-18 09:54:09 +0100

    add style and make content dynamic with more components
    Using @props to pass data to components
    
    Section 5 lesson 33 of laracasts from scratch tuto

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,17 +4,10 @@
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
 
-        <x-post-featured-card />
-
-        <div class="lg:grid lg:grid-cols-2">
-            <x-post-card />
-            <x-post-card />
-        </div>
-
-        <div class="lg:grid lg:grid-cols-3">
-            <x-post-card />
-            <x-post-card />
-            <x-post-card />
-        </div>
+        @if ($posts->count())
+            <x-posts-grid :posts="$posts" />
+        @else
+            <p>No posts yet. Please check back later.</p>
+        @endif
     </main>
 </x-layout>
