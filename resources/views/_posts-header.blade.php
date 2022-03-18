3bf233cf3ff0e978014bafbcf6a085e52cbfb9f1
jacques <hovine.jacques@gmail.com>
2022-03-18 14:41:17 +0100

    clean up files and refactor
    Section 5 lesson 36 of laracasts from scratch tuto

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -3,15 +3,7 @@
         Latest <span class="text-blue-500">Laravel From Scratch</span> News
     </h1>
 
-    <h2 class="inline-flex mt-2">By Lary Laracore <img src="/images/lary-head.svg"
-                                                       alt="Head of Lary the mascot"></h2>
-
-    <p class="text-sm mt-14">
-        Another year. Another update. We're refreshing the popular Laravel series with new content.
-        I'm going to keep you guys up to speed with what's going on!
-    </p>
-
-    <div class="space-y-2 lg:space-y-0 lg:space-x-4 mt-8">
+    <div class="space-y-2 lg:space-y-0 lg:space-x-4 mt-4">
         <!--  Category -->
         <div class="relative lg:inline-flex bg-gray-100 rounded-xl">
             <x-dropdown>
@@ -39,7 +31,7 @@
         </div>
 
         <!-- Other Filters -->
-        <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl">
+        {{-- <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl">
             <select class="flex-1 appearance-none bg-transparent py-2 pl-3 pr-9 text-sm font-semibold">
                 <option value="category" disabled selected>Other Filters
                 </option>
@@ -66,6 +58,6 @@
                 <input type="text" name="search" placeholder="Find something"
                        class="bg-transparent placeholder-black font-semibold text-sm">
             </form>
-        </div>
+        </div> --}}
     </div>
 </header>
