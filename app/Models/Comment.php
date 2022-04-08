5e4043fa9579ef648bccac1634424f7650eb2059
Jacques <jacques.hovine@eemi.com>
2022-04-08 12:13:06 +0200

    create a factory for comments and update relationships in models
    Section 10 lesson 54 of laracasts from scratch tuto

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     use HasFactory;
+
+    public function post()
+    {
+        // Here, Laravel will assume that the foreign key is post_id (by default)
+        return $this->belongsTo(Post::class);
+    }
+
+    public function author()
+    {
+        // The function does not have the same name as the class, hence why we need to specify the columns
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
