855e8cbab05a522086d8f6e0b34a09a7b80a9336
Jacques <jacques.hovine@eemi.com>
2022-04-08 15:05:17 +0200

    style comments and comments form
    the style from the div surrounding comments has been extracted to its
    own component. It could have been refactored with a simple css class,
    but it is better to write as little css as possible when using
    frameworks like tailwind.
    
    Section 10 lesson 55 of laracasts from scratch tuto

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -51,6 +51,31 @@
                 </div>
 
                 <section class="col-span-8 col-start-5 mt-10 space-y-6">
+                    <x-panel>
+                        <form method="POST" action="#">
+                            @csrf
+
+                            <header class="flex items-center">
+                                <img src="https://i.pravatar.cc/60?u={{ auth()->id() }}" alt="" width="60" height="60" class="rounded-full">
+                                <h2 class="ml-4">Want to participate ?</h2>
+                            </header>
+
+                            <div class="mt-6">
+                                <textarea 
+                                    name="body" 
+                                    class="w-full text-sm focus:outline-none focus:ring" 
+                                    rows="5" 
+                                    placeholder="Quick, think of something to say!"></textarea>
+                            </div>
+
+                            <div class="flex justify-end mt-6 border-t border-gray-200 pt-6">
+                                <button type="submit"
+                                        class="text-xs py-2 px-10 rounded-2xl bg-blue-500 text-white uppercase font-semibold hover:bg-blue-600">Post
+                                </button>
+                            </div>
+                        </form>
+                    </x-panel>
+
                     @foreach ($post->comments as $comment)
                         <x-post-comment :comment="$comment" />
                     @endforeach
