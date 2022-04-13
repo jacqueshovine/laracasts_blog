3b4a3ff23dae33fa5419ae77821e9821514497b5
Jacques <jacques.hovine@eemi.com>
2022-04-13 21:44:51 +0200

    refactor submit button using component

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -89,13 +89,7 @@
                     @enderror
                 </div>
 
-                <div class="mb-6">
-                    <button type="submit"
-                            class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500"
-                    >
-                        Submit
-                    </button>
-                </div>
+                <x-submit-button>Submit</x-submit-button>
 
                 <!-- If we want to display all errors at one place in a list -->
                 {{-- @if ($errors->any())
