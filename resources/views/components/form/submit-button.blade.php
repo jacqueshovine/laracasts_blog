7a9bc6f9c754000e6246115bf8b9dd9f63cea361
Jacques <jacques.hovine@eemi.com>
2022-04-17 13:55:28 +0200

    bugfix

--- resources/views/components/form/submit-button.blade.php
+++ resources/views/components/form/submit-button.blade.php
@@ -1,6 +1,6 @@
-<x-form-field>
+<x-form.field>
     <button type="submit"
     class="text-xs py-2 px-10 rounded-2xl bg-blue-500 text-white uppercase font-semibold hover:bg-blue-600">
     {{ $slot }}
     </button>
-</x-form-field>
+</x-form.field>
