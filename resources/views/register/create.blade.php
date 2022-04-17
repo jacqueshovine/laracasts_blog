4556c3f88e0ad6c596267e582d06b03706730122
Jacques <jacques.hovine@eemi.com>
2022-04-17 16:46:41 +0200

    refactor register form with input components
    Section 12 lesson 65 of laracasts from scratch tuto

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -4,90 +4,11 @@
             <h1 class="text-center font-bold text-xl">Register!</h1>
             <form method="POST" action="/register" class="mt-10">
                 @csrf
-                
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                           for="name"
-                    >
-                        Name
-                    </label>
 
-                    <!-- Setting the value as {{ old('name') }} prevents the need to refill every field when an error happens. -->
-                    <input class="border border-gray-400 p-2 w-full"
-                           type="text"
-                           name="name"
-                           id="name"
-                           value="{{ old('name') }}"
-                           required
-                    >
-
-                    @error('name')
-                    <!-- $message contains the validation error message -->
-                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                    @enderror
-                </div>
-
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                           for="username"
-                    >
-                        Username
-                    </label>
-
-                    <input class="border border-gray-400 p-2 w-full"
-                           type="text"
-                           name="username"
-                           id="username"
-                           value="{{ old('username') }}"
-                           required
-                    >
-
-                    @error('username')
-                    <!-- $message contains the validation error message -->
-                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                    @enderror
-                </div>
-
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                           for="email"
-                    >
-                        Email
-                    </label>
-
-                    <input class="border border-gray-400 p-2 w-full"
-                           type="email"
-                           name="email"
-                           id="email"
-                           value="{{ old('email') }}"
-                           required
-                    >
-
-                    @error('email')
-                    <!-- $message contains the validation error message -->
-                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                    @enderror
-                </div>
-
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                           for="password"
-                    >
-                        Password
-                    </label>
-
-                    <input class="border border-gray-400 p-2 w-full"
-                           type="password"
-                           name="password"
-                           id="password"
-                           required
-                    >
-
-                    @error('password')
-                    <!-- $message contains the validation error message -->
-                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                    @enderror
-                </div>
+                <x-form.input name="name"/>
+                <x-form.input name="username"/>
+                <x-form.input name="email" type="email"/>
+                <x-form.input name="password" type="password" autocomplete="new-password"/>
 
                 <x-form.submit-button>Submit</x-form.submit-button>
 
