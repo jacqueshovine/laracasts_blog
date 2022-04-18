fc683809d3497cac42e6493ab4c422a30d97d806
Jacques <jacques.hovine@eemi.com>
2022-04-18 11:50:16 +0200

    create admin gate for authorization
    create custom blade directive for admin access
    update layout view links with admin access
    remove no longer used MustBeAdministrator middleware
    refactor routes file with resource helper
    
    Section 12 lesson 69 of laracasts from scratch tuto

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,12 @@
 
 namespace App\Providers;
 
+use App\Models\User;
 use App\Services\MailChimpNewsletter;
 use App\Services\Newsletter;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 use MailchimpMarketing\ApiClient;
 
@@ -38,5 +41,15 @@ class AppServiceProvider extends ServiceProvider
     {
         // Disables mass assignment by default.
         // Model::unguard();
+
+        Gate::define('admin', function (User $user) {
+            // Determine if the currently signed in user satisfies this logic
+            return $user->email === 'jacko@duck.com';
+        });
+
+        // Create a custom blade directive @admin with its own logic
+        Blade::if('admin', function () {
+            return request()->user()?->can('admin'); // Referencing admin gate
+        });
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -47,12 +47,21 @@ Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth'
 
 
 // Admin
-Route::get('admin/posts/create', [AdminPostController::class, 'create'])->middleware('admin');
-Route::post('admin/posts/', [AdminPostController::class, 'store'])->middleware('admin');
-Route::get('admin/posts', [AdminPostController::class, 'index'])->middleware('admin');
-Route::get('admin/posts/{post}/edit', [AdminPostController::class, 'edit'])->middleware('admin');
-Route::patch('admin/posts/{post}', [AdminPostController::class, 'update'])->middleware('admin');
-Route::delete('admin/posts/{post}', [AdminPostController::class, 'destroy'])->middleware('admin');
+// All these routes use the same middleware
+Route::middleware('can:admin')->group(function () {
+
+    // There are 7 actions for a resourceful controller. We can reference them all with the route resource helper.
+    // In this case, we had no show action, so we exclude it.
+    Route::resource('admin/posts', AdminPostController::class)->except('show');
+
+    // Route::get('admin/posts/create', [AdminPostController::class, 'create']);
+    // Route::post('admin/posts/', [AdminPostController::class, 'store']);
+    // Route::get('admin/posts', [AdminPostController::class, 'index']);
+    // Route::get('admin/posts/{post}/edit', [AdminPostController::class, 'edit']);
+    // Route::patch('admin/posts/{post}', [AdminPostController::class, 'update']);
+    // Route::delete('admin/posts/{post}', [AdminPostController::class, 'destroy']);
+});
+
 
 // The syntax below would be used if we had not defined getRouteKeyName() function in the Post Model
 // Route::get('posts/{post:slug}', function (Post $post) { // Post::where('slug', $post)->firstOrFail()
